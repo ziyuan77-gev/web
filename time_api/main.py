998e71ea3bb336fe0e81e8bd8382006a6ea92242
xhdndmm <zhangzx1149@outlook.com>
2024-10-25 21:11:16 +0800

    Changes to be committed: 	modified:   index.html 	new file:   time_api/main.py

---
+++ time_api/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import ntplib
+from time import ctime
+
+app = Flask(__name__)
+
+@app.route('/ntp-time', methods=['GET'])
+def get_ntp_time():
+    try:
+        client = ntplib.NTPClient()
+        response = client.request('ntp.aliyun.com ')
+        current_time = ctime(response.tx_time)
+        return jsonify({"ntp_time": current_time})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
